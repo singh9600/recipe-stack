dd1e1bca887a931941a3e9ec74ba1be26850f282
Harpreet Singh <hsingh9600@gmail.com>
2020-09-04 06:22:41 +1200

    home page

--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="home">
-    <h1>RecipeSt<i class="fas fa-ice-cream"></i>ck</h1>
-    <p>Generate countless recipies and save the ones you like for later.</p>
-    <div class="inputs">
+    <div class="homeTop">
+      <h1>RecipeSt<i class="fas fa-ice-cream"></i>ck</h1>
+      <div class="steps">
+        <p>1) Login or Create a new account.</p>
+        <p>2) View countless randomly generated recipies.</p>
+        <p>3) Save the ones you like for later.</p>
+        <p><i class="fas fa-robot"></i>  AI suggestions coming soon!  <i class="fas fa-robot"></i></p>
+      </div>
       <input class="btn btn-primary" type="submit" value="Login">
       <input class="btn btn-primary" type="submit" value="SignUp">
     </div>
-    <img 
-      src="@/assets/MIXING BOWL 1.svg"
-      class="homeImg">
   </div>
 </template>
 
@@ -32,15 +34,33 @@ export default {
     font-size: 150%;
   }
   .home {
-    padding-top: 15%;
     text-align: center;
     color: #427286;
     font-family: 'Ubuntu';
+    display: grid;
+    height: 100vh;
+    grid-template-columns: 40px 50px auto 50px 40px;
+    grid-template-rows: 25% auto 20px;
+  }
+  .homeTop {
+    grid-column-start: 2;
+    grid-column-end: 5;
+    grid-row-start: 2;
+    grid-row-end: 2;
+  }
+  .steps {
+    padding-top: 20px;
+    padding-bottom: 20px;
   }
   .btn {
     margin: 8px !important;
   }
-  .fas {
+  .fa-ice-cream {
     font-size: 70%;
+  }
+  @media screen and (max-width: 460px) {
+    .home {
+      grid-template-rows: 15% auto 20px;
+    }
   }
 </style>
