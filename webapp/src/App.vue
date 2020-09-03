f5b182e0c568d2aa22335f7e7155788b76b2b64f
Harpreet Singh <hsingh9600@gmail.com>
2020-09-04 04:50:55 +1200

    init commit

---
+++ webapp/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  background-color: #A4D8E2;
+  height: 100vh;
+}
+</style>
---
+++ webapp/src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <h1>RecipeSt<i class="fas fa-ice-cream"></i>ck</h1>
+    <p>Generate countless recipies and save the ones you like for later.</p>
+    <div class="inputs">
+      <input class="btn btn-primary" type="submit" value="Login">
+      <input class="btn btn-primary" type="submit" value="SignUp">
+    </div>
+    <img 
+      src="@/assets/MIXING BOWL 1.svg"
+      class="homeImg">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    font-size: 300%;
+  }
+  p {
+    font-size: 150%;
+  }
+  .home {
+    padding-top: 15%;
+    text-align: center;
+    color: #427286;
+    font-family: 'Ubuntu';
+  }
+  .btn {
+    margin: 8px !important;
+  }
+  .fas {
+    font-size: 70%;
+  }
+</style>
